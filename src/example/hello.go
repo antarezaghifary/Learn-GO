68db7cb63551127a53bb7b7b9c9b0603383c6334
Antareza Ghifary A.M <oneakun08@gmail.com>
2019-09-11 12:57:49 +0700

    Commit Project GoLang

---
+++ src/example/hello.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	/*fmt.Println("Hello")
+	var x int = 5
+	var y int = 9
+	var sum int = x + y
+	x := 5
+	y := 9
+	sum := x + y
+
+	fmt.Println(sum)
+
+	x := 5
+	if x < 6 {
+		fmt.Println("BenarLah")
+	} else {
+		fmt.Println("SalahLah")
+	}*/
+
+	//var a [2]int
+	//a[5] = 6
+
+	//a := [3]int{1,2,3}
+	/*vertikal := make(map[string]int)
+	vertikal["setitiga"] = 2
+	vertikal["kipas"] = 3
+	vertikal["bunder"] = 12
+	*/
+	//delete(vertikal, "kipas")
+	/*i := 0
+	for i < 5 {
+		fmt.Println(i)
+		i++
+	}*/
+	/*arr := []string{"a","b","c"}
+	for index, value := range arr{
+		fmt.Println("index:", index, "value:", value)
+	}*/
+	/*m := make(map[string]string)
+	m["a"] = "alpha"
+	m["b"] = "beta"
+
+	for key, value := range m {
+		fmt.Println("key:", key, "value:", value )
+	}*/
+	//fmt.Println(vertikal ["kipas"])
+	/*result := sum(2, 3)
+	fmt.Println(result)*/
+	
+}
+
+	func sum(x int, y int) int{
+		return x * y
+	}
+
+
+
+
+
+
+
+
+
+
+
